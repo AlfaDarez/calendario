aebd61c9b176414e268970a3be8e52ce75d3bbaa
Jose Alfredo Valladares <josevalladaresvillanueva@gmail.com>
2024-02-12 16:43:27 -0600

    Trabajando Css

---
+++ src/Templates/calendario/calendario.js
@@ -0,0 +1,18 @@
+import '../css/calendario.css';
+
+function Calendario(){
+
+    return(
+        <div className='contenedor'>
+           <div className='caja1'></div>
+           <div className='caja2'></div>
+           <div className='caja3'></div>
+           <div className='caja4'></div>
+           <div className='caja5'></div>
+        </div>
+
+    );
+
+}
+
+export default Calendario;
